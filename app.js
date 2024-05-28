2f3fa80fa3ac33efd51a68b404aa3fb13962e82d
Lorena C <criado.manzaneque@gmail.com>
2024-05-28 21:01:43 +0200

    Create renderImgs method to made private the image attributes

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+const images = [
+    { id:'img-1', src: 'images/1.png', hasBike: true },
+    { id:'img-2', src: 'images/2.png', hasBike: true },
+    { id:'img-3', src: 'images/3.png', hasBike: true },
+    { id:'img-4', src: 'images/4.png', hasBike: false },
+    { id:'img-5', src: 'images/5.png', hasBike: false },
+    { id:'img-6', src: 'images/6.png', hasBike: true },
+    { id:'img-7', src: 'images/7.png', hasBike: false },
+    { id:'img-8', src: 'images/8.png', hasBike: false },
+    { id:'img-9', src: 'images/9.png', hasBike: false }
+]
+
+function renderImgs(){
+    const container = document.getElementById('images-container');
+    let row;
+
+    images.forEach((img, index) => {
+        if (index % 3 === 0) {
+            row = document.createElement('div');
+            row.classList.add('row', 'p-2', 'justify-content-center');
+            container.appendChild(row);
+        }
+
+        const imgElement = document.createElement('img');
+        imgElement.id = img.id;
+        imgElement.src = img.src;
+        imgElement.hasBike = img.hasBike;
+        imgElement.classList.add('rounded', 'float-start', 'w-25', 'col-sm-');
+        imgElement.addEventListener('click', () => this.selectImg(imgElement));
+        row.appendChild(imgElement);
+    });
+}
+
 let selectedImages = []
 
 function selectImg(img){
@@ -14,7 +47,7 @@ function checkSelectedImages(selectedImages){
    let incorrectImages = 0
    selectedImages.forEach(imgId => {
     const img = document.getElementById(imgId)
-        if (img.classList.contains('bike-img')) {
+        if (img.hasBike === true) {
             imagesHaveBikeImage.push(imgId)
         } else {
             incorrectImages++
@@ -36,12 +69,4 @@ function resetSelectedImages(){
     selectedImages = []
 }
 
-const imageElements = document.querySelectorAll("[data-img]")
-
-imageElements.forEach(img => {
-    img.addEventListener("click", () => {
-        selectImg(img)
-    })
-})
-
-
+document.addEventListener("DOMContentLoaded", renderImgs())
