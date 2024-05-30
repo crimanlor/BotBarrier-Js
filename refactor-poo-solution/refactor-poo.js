3bd3e5b34769d436b251d7924d40f9c208b38d5d
Lorena C <criado.manzaneque@gmail.com>
2024-05-30 12:21:01 +0200

    Add images array at JS file and create a render functionality

--- refactor-poo-solution/refactor-poo.js
+++ refactor-poo-solution/refactor-poo.js
@@ -1,22 +1,45 @@
 class Captcha {
     constructor(){
+        this.images = [
+            { id:'img-1', src: 'images/1.png', hasBike: true },
+            { id:'img-2', src: 'images/2.png', hasBike: true },
+            { id:'img-3', src: 'images/3.png', hasBike: true },
+            { id:'img-4', src: 'images/4.png', hasBike: false },
+            { id:'img-5', src: 'images/5.png', hasBike: false },
+            { id:'img-6', src: 'images/6.png', hasBike: true },
+            { id:'img-7', src: 'images/7.png', hasBike: false },
+            { id:'img-8', src: 'images/8.png', hasBike: false },
+            { id:'img-9', src: 'images/9.png', hasBike: false }
+        ]
         this.selectedImages = []
-        this.imageElements = document.querySelectorAll("[data-img]")
-        this.addClickListeners()
+        this.renderImgs()
     }
 
-    addClickListeners(){
-        this.imageElements.forEach(img => {
-            img.addEventListener("click", () => {
-                this.selectImg(img)
-            })
-        })
+    renderImgs(){
+        const container = document.getElementById('images-container');
+        let row;
+    
+        this.images.forEach((img, index) => {
+            if (index % 3 === 0) {
+                row = document.createElement('div');
+                row.classList.add('row', 'p-2', 'justify-content-center');
+                container.appendChild(row);
+            }
+    
+            const imgElement = document.createElement('img');
+            imgElement.id = img.id;
+            imgElement.src = img.src;
+            imgElement.hasBike = img.hasBike;
+            imgElement.classList.add('rounded', 'float-start', 'w-25', 'col-sm-');
+            imgElement.addEventListener('click', () => this.selectImg(imgElement));
+            row.appendChild(imgElement);
+        });
     }
 
-    selectImg(img){
-        if (!this.selectedImages.includes(img.id)){
-            img.style.opacity = 0.7;
-            this.selectedImages.push(img.id)
+    selectImg(imgElement){
+        if (!this.selectedImages.includes(imgElement.id)){
+            imgElement.style.opacity = 0.7;
+            this.selectedImages.push(imgElement.id)
         } else {
             window.alert("This image has been selected before")
         } 
@@ -27,7 +50,7 @@ class Captcha {
         let incorrectImages = 0
         this.selectedImages.forEach(imgId => {
             const img = document.getElementById(imgId)
-            if (img.classList.contains('bike-img')) {
+            if (img.hasBike === true) {
                 imagesHaveBikeImage.push(imgId)
             } else {
                 incorrectImages++
@@ -50,6 +73,7 @@ class Captcha {
         this.selectedImages = []
     }
 }
+
 
 const captcha = new Captcha()
 
